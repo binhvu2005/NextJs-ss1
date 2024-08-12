07ac6d112553807c9ddfa6a9e2d3b9859e01ebad
binhvu2005 <147390616+binhvu2005@users.noreply.github.com>
2024-08-12 10:06:48 +0700

    ss1

---
+++ app/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+       <h1 style={{backgroundColor: 'blue', color: 'white', textAlign: 'center', padding: '10px'}}>footer</h1>
+    </div>
+  )
+}
---
+++ app/layouts/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1 style={{backgroundColor: 'red', color: 'white', textAlign: 'center', padding: '10px'}}>Header</h1>
+      <h2>trang chủ</h2>
+    </div>
+  )
+}
